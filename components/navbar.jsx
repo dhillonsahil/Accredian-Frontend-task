6baa739e52dce582ec71766a11ab4b5f178d0a38
Sahil <sahil2742002@gmail.com>
2024-07-19 08:16:42 +0530

    navbar fix

--- components/navbar.jsx
+++ components/navbar.jsx
@@ -7,19 +7,11 @@ const Navbar = () => {
   return (
     <nav className="bg-white border-gray-200 dark:bg-gray-900">
       <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-        <a
-          href="https://flowbite.com/"
-          className="flex items-center space-x-3 rtl:space-x-reverse"
-        >
-          <img
-            src="https://flowbite.com/docs/images/logo.svg"
-            className="h-8"
-            alt="Flowbite Logo"
-          />
+        <p className="flex items-center space-x-3 rtl:space-x-reverse text-blue-600">
           <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
-            Flowbite
+            accredian
           </span>
-        </a>
+        </p>
         <button
           onClick={() => setShowNavbar(!showNavbar)}
           data-collapse-toggle="navbar-default"
